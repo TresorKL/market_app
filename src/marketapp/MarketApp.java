3cea0326fa85c4bedf438c5a7a084be1a66e2ea6
TresorKL <trezmathieu@gmail.com>
2022-02-14 13:22:25 +0200

    main version

---
+++ src/marketapp/MarketApp.java
@@ -0,0 +1,17 @@
+
+package marketapp;
+
+/**
+ *
+ * @author tresorkl
+ */
+public class MarketApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("hello world");
+    }
+    
+}
