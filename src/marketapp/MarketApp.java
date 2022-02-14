e5a688cc10f6191eeb6c7cb4862a72a1a2855ab2
NorbertNzazi <nzazinorberto@gmail.com>
2022-02-14 14:33:52 +0200

    my first commit

--- src/marketapp/MarketApp.java
+++ src/marketapp/MarketApp.java
@@ -11,7 +11,10 @@ public class MarketApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         System.out.print("hello world");
+        System.out.println("Hello Norbert");
+                    
     }
     
 }
